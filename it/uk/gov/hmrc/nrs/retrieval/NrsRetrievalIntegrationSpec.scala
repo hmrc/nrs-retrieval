046bf9f88053dd6e47a377c4f6e56548ccf8adaf
PaulHodgson <paul.hodgson073@gmail.com>
2021-06-29 13:00:42 +0100

    [NONPR-2134] error for unexpected status code for get submisison bundle

--- it/uk/gov/hmrc/nrs/retrieval/NrsRetrievalIntegrationSpec.scala
+++ it/uk/gov/hmrc/nrs/retrieval/NrsRetrievalIntegrationSpec.scala
@@ -223,17 +223,17 @@ class NrsRetrievalIntegrationSpec
     val requestWithHeader = wsClientWithXApiKeyHeader(url)
     val retrievalPath = "/retrieval/submission-bundles/vaultId/archiveId"
 
-    def givenSubmissionBundlesReturns(status: Int, withHeaders: Boolean): Unit =
+    def givenSubmissionBundlesReturns(status: Int, withHeaders: Boolean, body: String = "some bytes"): Unit =
       wireMockServer.addStubMapping(
         mappingBuilder(get(urlPathMatching(retrievalPath)), withHeaders)
-          .willReturn(aResponse().withStatus(status)).build())
+          .willReturn(aResponse().withStatus(status).withBody(body)).build())
 
     Seq(
       (requestWithHeader, true, "the X-API-Key header"),
       (requestWithoutHeaders, false, "no headers")
     ).foreach { case (request, withHeaders, headersLabel) =>
       "pass through the X-API-Key header if set and return OK" when {
-        Seq(OK, CREATED, ACCEPTED, BAD_REQUEST, UNAUTHORIZED, FORBIDDEN, NOT_FOUND, INTERNAL_SERVER_ERROR, BAD_GATEWAY).foreach { status =>
+        Seq(OK, CREATED, ACCEPTED).foreach { status =>
           s"the request has $headersLabel and the submission bundles request returns the status $status" in {
             givenSubmissionBundlesReturns(status, withHeaders)
             request.get().futureValue.status shouldBe OK
@@ -245,6 +245,34 @@ class NrsRetrievalIntegrationSpec
           givenRequestRedirects(get(urlPathMatching(retrievalPath)), withHeaders)
           request.get().futureValue.status shouldBe OK
           verify(1, getRequestedFor(urlEqualTo(retrievalPath)))
+        }
+      }
+
+      "pass through the X-API-Key header if set and return NOT_FOUND" when {
+        s"the request has $headersLabel and the submission bundles request returns NOT_FOUND" in {
+          givenSubmissionBundlesReturns(NOT_FOUND, withHeaders)
+          request.get().futureValue.status shouldBe NOT_FOUND
+          verify(1, getRequestedFor(urlEqualTo(retrievalPath)))
+        }
+      }
+
+      "pass through the X-API-Key header if set and return INTERNAL_SERVER_ERROR" when {
+        Seq(BAD_REQUEST, UNAUTHORIZED, FORBIDDEN).foreach { status =>
+          s"the request has $headersLabel and the submission bundles request returns the status $status" in {
+            givenSubmissionBundlesReturns(status, withHeaders)
+            request.get().futureValue.status shouldBe INTERNAL_SERVER_ERROR
+            verify(1, getRequestedFor(urlEqualTo(retrievalPath)))
+          }
+        }
+      }
+
+      "pass through the X-API-Key header if set and return BAD_GATEWAY" when {
+        Seq(INTERNAL_SERVER_ERROR, BAD_GATEWAY).foreach { status =>
+          s"the request has $headersLabel and the submission bundles request returns the status $status" in {
+            givenSubmissionBundlesReturns(status, withHeaders)
+            request.get().futureValue.status shouldBe BAD_GATEWAY
+            verify(1, getRequestedFor(urlEqualTo(retrievalPath)))
+          }
         }
       }
     }
